4efe6e2e84adc25c9994ff338745218c0752f279
Robin Millette <robin@millette.info>
2016-05-31 16:40:07 -0400

    Pass strategy options thru manifest.json.

--- plugins/login/index.js
+++ plugins/login/index.js
@@ -1,19 +1,11 @@
 'use strict'
 
-// Terrible idea but required if not using HTTPS especially if developing locally
-const secure = false
-const cookiePassword = 'cookie_encryption_password_secure'
-
 exports.register = (server, pluginOptions, next) => {
   const mode = pluginOptions && pluginOptions.auth && pluginOptions.auth.mode
-  const options = {
-    password: cookiePassword,
-    isSecure: secure
-  }
   if (mode) {
-    server.auth.strategy('session', 'cookie', mode, options)
+    server.auth.strategy('session', 'cookie', mode, pluginOptions.strategy)
   } else {
-    server.auth.strategy('session', 'cookie', options)
+    server.auth.strategy('session', 'cookie', pluginOptions.strategy)
   }
 
   next()
