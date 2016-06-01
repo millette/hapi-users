59ba43966305f8c6dafbf34534305d5dea34b388
Robin Millette <robin@millette.info>
2016-06-01 19:49:02 -0400

    Remove session cookie if user is removed from db.

--- plugins/login/index.js
+++ plugins/login/index.js
@@ -3,6 +3,20 @@
 exports.register = (server, pluginOptions, next) => {
   const mode = pluginOptions && pluginOptions.auth && pluginOptions.auth.mode
 
+  const validate = (request, session, callback) => {
+    const Users = request.collections.users
+    Users.findOne(session.id)
+      .then((x) => {
+        if (x) {
+          callback(null, true)
+        } else {
+          callback(null, false, {})
+        }
+      })
+  }
+
+  pluginOptions.strategy.validateFunc = validate
+
   if (mode) {
     server.auth.strategy('session', 'cookie', mode, pluginOptions.strategy)
   } else {
